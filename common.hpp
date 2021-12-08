54550cb5cc76ff4651c496ff32b7026a01e8f625
galoperidol <warriorofkatarin@gmail.com>
2021-12-08 15:22:21 +0300

    __vec_impl to common static array (_ct::s_array)

--- common.hpp
+++ common.hpp
@@ -2,6 +2,7 @@
 #define VX_COMMON_HPP
 
 #include <utility>
+#include <array>
 
 #ifdef VX_USE_BOOST
 #include <boost/utility.hpp>
@@ -75,22 +76,97 @@ struct types_pack
 };
 
 
-template<long ..._Args>
-struct long_sequence_pack
+template<size_t ..._Args>
+struct size_sequence_pack
 {
-	using sum = detail::__sum<long, _Args...>;
+	using sum = detail::__sum<size_t, _Args...>;
 };
 
 
-template<long _N, long... _Indices>
+template<size_t _N, size_t... _Indices>
 struct index_sequence_make : index_sequence_make<_N - 1, _N - 1, _Indices...> {};
-template<long... _Indices>
-struct index_sequence_make<0, _Indices...> : long_sequence_pack<_Indices...> {};
+template<size_t... _Indices>
+struct index_sequence_make<0, _Indices...> : size_sequence_pack<_Indices...> {};
 
-template<long _N, long... _Values>
+template<size_t _N, size_t... _Values>
 struct ones_sequence_make : ones_sequence_make<_N - 1, 1, _Values...> {};
-template<long... _Values>
-struct ones_sequence_make<0, _Values...> : long_sequence_pack<_Values...> {};
+template<size_t... _Values>
+struct ones_sequence_make<0, _Values...> : size_sequence_pack<_Values...> {};
+
+
+namespace _ct
+{
+
+template<typename _T, size_t _N>
+struct s_array : std::array<_T, _N>
+{
+	using base_type       = std::array<_T, _N>;
+	using value_type      = typename base_type::value_type;
+	using reference       = typename base_type::reference;
+	using const_reference = typename base_type::const_reference;
+
+	template<size_t _M>
+	constexpr reference get(void) noexcept { return std::get<_M>(*this); }
+	template<size_t _M>
+	constexpr const_reference get(void) const noexcept { return std::get<_M>(*this); }
+
+	constexpr explicit s_array(void) noexcept {}
+
+	constexpr explicit s_array(value_type _v) noexcept
+		: s_array(_v, ones_sequence_make<_N>{}) {}
+
+	template<size_t _M, typename = std::enable_if_t<(_N >= _M)>>
+	constexpr explicit s_array(value_type const (&_ar)[_M]) noexcept
+		: s_array(_ar, index_sequence_make<_M>{}) {}
+
+	template<size_t _M, size_t _L, typename = std::enable_if_t<(_N >= _M + _L)>>
+	constexpr explicit s_array(
+		s_array<value_type, _M> const & _v1, s_array<value_type, _L> const & _v2) noexcept
+		: s_array(_v1, index_sequence_make<_M>{}, _v2, index_sequence_make<_L>{}) {}
+
+	template<size_t _M, size_t... _Args,
+		typename = std::enable_if_t<_N >= size_sequence_pack<_M, _Args...>::sum::value>>
+	constexpr explicit s_array(s_array<value_type, _M> const & _first,
+		s_array<value_type, _Args> const &... _other) noexcept
+		: s_array(_first, s_array<value_type, size_sequence_pack<_Args...>::sum::value>(_other...)) {}
+
+	template<size_t _M, typename = std::enable_if_t<(_M > _N)>>
+	constexpr explicit s_array(s_array<value_type, _M> const & _v) noexcept
+		: s_array(_v, index_sequence_make<_N>{}) {}
+
+	template<typename _U>
+	constexpr explicit s_array(s_array<_U, _N> const & _v) noexcept
+		: s_array(_v, index_sequence_make<_N>{}) {}
+
+protected:
+
+	template<size_t... _Values>
+	constexpr explicit s_array(value_type _v, size_sequence_pack<_Values...>) noexcept
+		: base_type{static_cast<value_type>(_v * static_cast<value_type>(_Values))...} {}
+
+	template<size_t _M, size_t... _Indices>
+	constexpr explicit s_array(value_type const (&_ar)[_M], size_sequence_pack<_Indices...>) noexcept
+		: base_type{_ar[_Indices]...} {}
+
+	template<size_t _M, size_t _L, size_t... _Indices1, size_t... _Indices2>
+	constexpr explicit s_array(
+		s_array<value_type, _M> const & _v1, size_sequence_pack<_Indices1...>,
+		s_array<value_type, _L> const & _v2, size_sequence_pack<_Indices2...>) noexcept
+		: base_type{_v1.template get<_Indices1>()..., _v2.template get<_Indices2>()...} {}
+
+	template<size_t _M, size_t... _Indices, typename = std::enable_if_t<(_M > _N)>>
+	constexpr explicit s_array(s_array<value_type, _M> const & _v, size_sequence_pack<_Indices...>) noexcept
+		: base_type({_v.template get<_Indices>()...}) {}
+
+	template<typename _U, size_t... _Indices>
+	constexpr explicit s_array(s_array<_U, _N> const & _v, size_sequence_pack<_Indices...>) noexcept
+		: base_type({_v.template get<_Indices>()...}) {}
+};
+
+template<size_t _N>
+using s_string = s_array<char, _N>;
+
+}
 
 
 } // namespace vx
--- vec.hpp
+++ vec.hpp
@@ -15,84 +15,18 @@
 namespace vx
 {
 
-template<typename _T, size_t _N>
-struct __vec_impl : std::array<_T, _N>
-{
-	using base_type       = std::array<_T, _N>;
-	using value_type      = typename base_type::value_type;
-	using reference       = typename base_type::reference;
-	using const_reference = typename base_type::const_reference;
-
-	template<size_t _M>
-	constexpr reference get(void) noexcept { return std::get<_M>(*this); }
-	template<size_t _M>
-	constexpr const_reference get(void) const noexcept { return std::get<_M>(*this); }
-
-	constexpr explicit __vec_impl(void) noexcept {}
-
-	template<size_t... _Values>
-	constexpr explicit __vec_impl(value_type _v, long_sequence_pack<_Values...>) noexcept
-		: base_type{(_v * static_cast<value_type>(_Values))...} {}
-
-	constexpr explicit __vec_impl(value_type _v) noexcept
-		: __vec_impl(_v, ones_sequence_make<_N>{}) {}
-
-	template<size_t... _Indices>
-	constexpr explicit __vec_impl(value_type const (&_ar)[_N], long_sequence_pack<_Indices...>) noexcept
-		: base_type{_ar[_Indices]...} {}
-
-	constexpr explicit __vec_impl(value_type const (&_ar)[_N]) noexcept
-		: __vec_impl(_ar, index_sequence_make<_N>{}) {}
-
-
-	template<size_t _M, size_t... _Indices1, size_t... _Indices2>
-	constexpr explicit __vec_impl(
-		__vec_impl<value_type, _M> const & _v1, long_sequence_pack<_Indices1...>,
-		__vec_impl<value_type, _N - _M> const & _v2, long_sequence_pack<_Indices2...>) noexcept
-		: base_type{_v1.template get<_Indices1>()..., _v2.template get<_Indices2>()...} {}
-
-	template<size_t _M>
-	constexpr explicit __vec_impl(
-		__vec_impl<value_type, _M> const & _v1, __vec_impl<value_type, _N - _M> const & _v2) noexcept
-		: __vec_impl(_v1, index_sequence_make<_M>{}, _v2, index_sequence_make<_N - _M>{}) {}
-
-	template<size_t _M, size_t... _Args,
-		typename = std::enable_if_t<_N == long_sequence_pack<_M, _Args...>::sum::value>>
-	constexpr explicit __vec_impl(__vec_impl<value_type, _M> const & _first,
-		__vec_impl<value_type, _Args> const &... _other) noexcept
-		: __vec_impl(_first, __vec_impl<value_type, long_sequence_pack<_Args...>::sum::value>(_other...)) {}
-
-	template<size_t _M, size_t... _Indices, typename = std::enable_if_t<(_M > _N)>>
-	constexpr explicit __vec_impl(__vec_impl<value_type, _M> const & _v, long_sequence_pack<_Indices...>) noexcept
-		: base_type({_v.template get<_Indices>()...}) {}
-
-	template<size_t _M, typename = std::enable_if_t<(_M > _N)>>
-	constexpr explicit __vec_impl(__vec_impl<value_type, _M> const & _v) noexcept
-		: __vec_impl(_v, index_sequence_make<_N>{}) {}
-
-	template<typename _U, size_t... _Indices>
-	constexpr explicit __vec_impl(__vec_impl<_U, _N> const & _v, long_sequence_pack<_Indices...>) noexcept
-		: base_type({_v.template get<_Indices>()...}) {}
-
-	template<typename _U>
-	constexpr explicit __vec_impl(__vec_impl<_U, _N> const & _v) noexcept
-		: __vec_impl(_v, index_sequence_make<_N>{}) {}
-
-};
-
-
 template<typename _T, size_t _N,
 	typename = std::enable_if_t<(_N > 0 && _N <= 4) && std::is_arithmetic<_T>::value>>
-struct vec : __vec_impl<_T, _N>
+struct vec : _ct::s_array<_T, _N>
 {
-	using base_type  = __vec_impl<_T, _N>;
+	using base_type  = _ct::s_array<_T, _N>;
 	using value_type = typename base_type::value_type;
 	using self_type  = vec<value_type, _N>;
 
 	constexpr explicit vec(void) noexcept {}
 
 	template<size_t _M, size_t... _Args,
-		typename = std::enable_if_t<_N == long_sequence_pack<_M, _Args...>::sum::value>>
+		typename = std::enable_if_t<_N == size_sequence_pack<_M, _Args...>::sum::value>>
 	constexpr explicit vec(vec<value_type, _M> const & _first,
 		vec<value_type, _Args> const &... _other) noexcept
 		: base_type(_first, _other...) {}
